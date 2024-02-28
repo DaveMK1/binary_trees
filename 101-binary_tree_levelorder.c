a483b35d06e1da02a9abdbed585a9fc23be9bb2b
David Agbesinyale <davidm7040@gmail.com>
2024-02-28 09:56:32 -0800

    Level-order traversal

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,8 +1,27 @@
 #include "binary_trees.h"
 #include "9-binary_tree_height.c"
 
-void perform_on_level(const binary_tree_t *tree, size_t level,
-		void (*func)(int))
+/**
+ * binary_tree_height - measures the height of a binary tree from a given node
+ *
+ * @tree: tree to measure
+ *
+ * Return: 0 if the tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t lef = 0, rig = 0;
+
+		lef = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		rig = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+		return ((lef > rig) ? lef : rig);
+	}
+
+	return (0);
+}
 
 /**
  * binary_tree_levelorder - goes through a binary tree using
